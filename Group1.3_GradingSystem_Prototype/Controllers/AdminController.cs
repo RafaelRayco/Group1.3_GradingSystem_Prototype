2ef3f679426825400334bc7435654baf9bf98937
Rafael Rayco <rayco.rafaeljoseph.05032003@gmail.com>
2023-10-08 21:05:56 +0800

    Update admin pages

--- Group1.3_GradingSystem_Prototype/Controllers/AdminController.cs
+++ Group1.3_GradingSystem_Prototype/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Group1._3_GradingSystem_Prototype.Models;
+
 
 namespace Group1._3_GradingSystem_Prototype.Controllers
 {
@@ -6,6 +8,16 @@ namespace Group1._3_GradingSystem_Prototype.Controllers
     {
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminHomePage()
+        {
+            return View();
+        }
+
+        public ActionResult AdminDashboard()
         {
             return View();
         }
---
+++ Group1.3_GradingSystem_Prototype/Models/AdminModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Group1._3_GradingSystem_Prototype.Models
+{
+    public class AdminModel
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
